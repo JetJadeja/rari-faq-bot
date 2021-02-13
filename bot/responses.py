84a234738656b29f16835670c80a9e41204cd54f
JetDeveloping <jetjadeja@gmail.com>
2021-02-12 21:30:01 -0800

    Wrote responses

---
+++ bot/responses.py
@@ -0,0 +1,28 @@
+response = {
+    "helpNeeded": "You've come to the right place! Feel free to just ask your question, and I'll try my best to answer it!",
+
+    "stakeTimeline": 'We actually passed a proposal for a staking mechanism (RIP-4). This is currently being audited, but you can read about it here: https://snapshot.page/#/rari/proposal/QmPAA157pYkq3gUvM9LhGirGfe3nqnrCb5eT4hSEJs8gMs',
+    "liquidityMining": "Currently, we are in between farming periods, with the liquidity mining code being audited. It will be out soon. \n Read the Proposal here: https://snapshot.page/#/rari/proposal/QmU3334PXSAGd3kiSZo4FZkKPpNL5qvHiNwRZVTKkcCvv9",
+    
+    "rariExplainer": "You can read about the Rari Protocol here: https://www.notion.so/What-is-Rari-869c8a46e76f4558836d4f923083365c",
+    "fuseExplainer": 'Fuse is going to be a market of money markets. \n Watch the explanation here: https://www.youtube.com/watch?v=pK6Y_ZVRa9s&t=12m12s, and/or read the article here: https://medium.com/rari-capital/a-prelude-to-fuse-f0b3afab40c',
+    "tankExplainer": "Tanks allow you to lend one asset, and borrow another, maintaing your collateral ratio, earning yield on both assets. \n Watch the explainer here: https://www.youtube.com/watch?v=pK6Y_ZVRa9s&t=13m40s",
+    "airlockExplainer": "A cross layer AMM. More information to come",
+
+    "notion": "https://www.notion.so/Rari-Capital-3d762a07d2c9417e9cd8c2e4f719e4c3",
+    "team": "Learn about the team here (and don't forget to follow them on twitter!): https://www.notion.so/Rari-Team-Contributors-b37606325e954922bd3b6cfd2715758d",
+    "addresses": "Check out the important addresses here: https://www.notion.so/Important-Addresses-84a1ea063dc14c7dba750527f8672840",
+
+    "priceTalk": "Unfortunately, this question is impossible to answer.",
+}
+
+greetings = [
+    "Hey there!",
+    "Hello!",
+    "Great question!"
+    "Hey!",
+    "Neat question!",
+    "Thanks for asking!",
+    "Hello there!",
+    "I've been waiting for this question!"
+]
